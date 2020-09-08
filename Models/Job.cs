cba80fcd2244f27c956d096c5bd10637929adfe4
Klement Baastrup Johansen <kljo@aspit.dk>
2020-09-08 14:43:59 +0200

    Add Job data model

---
+++ Models/Job.cs
@@ -0,0 +1,10 @@
+namespace Core_REST_API_les_jackson_course.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+    }
+}
