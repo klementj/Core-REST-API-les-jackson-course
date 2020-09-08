e141dc821115b0a4ec5641da5b6e01308e8ec2ad
Klement Baastrup Johansen <kljo@aspit.dk>
2020-09-08 14:54:53 +0200

    Dependency injection of MockJobsRepo

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core_REST_API_les_jackson_course.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace Core_REST_API_les_jackson_course
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<IJobsRepo, MockJobsRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
